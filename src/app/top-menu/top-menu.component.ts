cafdf8bd7039fcf343180bfec442fa93f164cd38
lebris <corentin.le.bris1@gmail.com>
2019-03-20 15:33:26 +0100

    home

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-menu',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-menu.component.css']
 })
 export class TopMenuComponent implements OnInit {
-
-  constructor() { }
+  isConnected:boolean
+  router: Router;
+  constructor(router: Router) {
+    this.router = router;
+   }
 
   ngOnInit() {
+    if(localStorage.getItem('token')) {
+      this.isConnected = true
+    } else {
+      this.isConnected = false
+    }
+    
   }
-
+   diconnect() {
+     localStorage.removeItem('token')
+     this.router.navigate(["/login"]);
+   }
 }
